b57cba0f23c13772c4399de888c57a4ea4e54fc8
Vedhavathi Y A <vedha9782@gmail.com>
2026-03-23 22:40:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Day 1: JavaScript Basics
+
+// Variables
+let message = "You clicked the button! 🎉";
+
+// Function
+function showMessage() {
+    document.getElementById("message").innerText = message;
+    console.log("Button was clicked!");
+}
+
+// Console output
+console.log("JavaScript is connected successfully!");
